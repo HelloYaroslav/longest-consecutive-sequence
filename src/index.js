65e88c8159cd4ecff161852d8cb474450ba51da0
HelloYaroslav <yaroslav.klimenkovv@gmail.com>
2018-03-05 18:59:52 +0300

    8

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+module.exports =
+    function longestConsecutiveLength(array) {
+        let clone = [];
+        let result = 0;
+        let bufResult = 0;
+        if (array.length == 1)
+            return 1;
+
+        for (let i = 0; i < array.length; i++)
+            clone[array[i]] = array[i];
+
+        for (i = 0; i < clone.length; i++) {
+            if (typeof(clone[i]) !== 'undefined')
+                bufResult++;
+            else {
+                if (result < bufResult)
+                    result = bufResult;
+                bufResult = 0;
+            }
+
+        }
+
+        return result;
+
+
+    }
